ff438f892b8c5d91940f4a9bdce092f8d2fd6a04
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2023-03-03 12:10:59 +0530

    each method

---
+++ custom_each/my_each.rb
@@ -0,0 +1,17 @@
+class Array
+	def my_each(&block)
+		return to_enum(:my_each) unless block_given?
+
+		i = 0
+		while i < size
+			yield self[i]
+			i += 1
+		end
+		self
+	end
+end
+
+arr = [1,3,4,8,9,6,2]
+arr.my_each do |i|
+	print i, " "
+end
